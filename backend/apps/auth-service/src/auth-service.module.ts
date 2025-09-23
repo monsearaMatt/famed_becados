eade16da21b8574e1b0ef633dc73cf943e2fe727
tomasplz <tomasplz@hotmail.com>
2025-09-22 23:52:04 -0300

    feat(auth-service): agregados los modulos AuthModule y UsersModule a la estructura del proyecto, particularmente para el microservicio de autentificación

--- backend/apps/auth-service/src/auth-service.module.ts
+++ backend/apps/auth-service/src/auth-service.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthServiceController } from './auth-service.controller';
 import { AuthServiceService } from './auth-service.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule],
   controllers: [AuthServiceController],
   providers: [AuthServiceService],
 })
