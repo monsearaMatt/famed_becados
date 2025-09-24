ff06ec8c2a4451e632313af49e8f8c9f10e510ef
Ahito-09 <eduardo.macaya@alumnos.ucn.cl>
2025-09-24 00:05:51 -0300

    Update layout.tsx

--- frontend_pc/app/layout.tsx
+++ frontend_pc/app/layout.tsx
@@ -28,6 +28,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div> 
+          
+
+        </div>
+
+
         {children}
       </body>
     </html>
