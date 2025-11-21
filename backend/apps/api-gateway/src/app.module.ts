ed36fd1fdb4c6827a6923caa9dbf6a1dbe4275d1
Tomas Vargas <97796331+tomasplz@users.noreply.github.com>
2025-11-21 20:09:13 -0300

    Delete backend directory

--- backend/apps/api-gateway/src/app.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
-import { NatsModule } from './transport/nats.module';
-
-@Module({
-  imports: [AuthModule, NatsModule],
-})
-export class AppModule {}
